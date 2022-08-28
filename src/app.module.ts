c9d907bd8e4a74c3922be2e6ad22220742db7d79
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-08-28 10:45:14 +0200

    Conectando Nest con Mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { join } from 'path'; // en Node
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
@@ -9,6 +11,10 @@ import { PokemonModule } from './pokemon/pokemon.module';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+
+    // Referencia a la BBDD. Luego se hará con variables de entorno
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+
     PokemonModule,
   ],
 })
