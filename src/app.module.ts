a68ccdad0f77a12505874332360a5761a04becce
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-08-28 10:14:40 +0200

    Añadido módulo Pokemon

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
 import { join } from 'path'; // en Node
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
   imports: [
     // Con esto se pueden desplegar aplicaciones de React, Angular...
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
-  })],
+    }),
+    PokemonModule,
+  ],
 })
 export class AppModule {}
