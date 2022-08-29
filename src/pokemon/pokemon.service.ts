bc343c89cefd26dd2c2873645b93fe99fff48448
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-08-29 17:52:27 +0200

    Tratar duplicado en update

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -81,14 +81,20 @@ export class PokemonService {
     if (updatePokemonDto.name)
       updatePokemonDto.name = updatePokemonDto.name.toLocaleLowerCase();
 
-    // Con new: true devolveríamos el nuevo objeto. Si no se pone regresa el valor anterior.
-    // Pero no funciona
-    //await pokemon.updateOne(updatePokemonDto, { new: true });
-    await pokemon.updateOne(updatePokemonDto);
+    try {
+      await pokemon.updateOne(updatePokemonDto);
+      return { ...pokemon.toJSON(), ...updatePokemonDto };
+    } catch (error) {
+      if (error.code === 11000)
+        throw new BadRequestException(
+          `Pokemon exists in db ${JSON.stringify(error.keyValue)}`,
+        );
 
-    // En vez del new: true devolvemos el pokemon actualizado y sobreescribimos los
-    // valores con updatePokemonDto
-    return { ...pokemon.toJSON(), ...updatePokemonDto };
+      console.log(error);
+      throw new InternalServerErrorException(
+        `Can't create Pokemon - Check server logs`,
+      );
+    }
   }
 
   remove(id: number) {
