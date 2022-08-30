1d6d1c496a03924af6edb3053c98535bdd16f64a
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-08-30 18:17:17 +0200

    Insertar múltiples registros simultaneamente. Forma 1 con Promesas

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -8,37 +8,36 @@ import { Model } from 'mongoose';
 
 @Injectable()
 export class SeedService {
-  // FetchApi solo funciona en la versión de Node 18 o superior.
-  // Se va a usar axios
-  //
-  // Dependencia no inyectada, pero así sabemos que hay una dependencia.
-  // Por ahora lo dejamos así, pero la idea es implementar inyección de dependencia
-  // para poder sustituir esta dependencia de axios por request o por FetchApi o cualquier
-  // otro paquete que nos sirva para realizar peticiones HTTP (patrón Adaptador)
   private readonly axios: AxiosInstance = axios;
 
   constructor(
-    // Inyecciones de dependencia. Se inyectan automáticamente.
-    // La instancia es la misma en toda la aplicación por defecto.
-    //
-    // Si se hubiera exportado PokemonService esto sería
-    //private readonly pokemonService: PokemonService;
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
   ) {}
 
   async executeSeed() {
+    // Borramos todo antes de insertar para evitar insertar duplicados
+    await this.pokemonModel.deleteMany();
+
     const { data } = await this.axios.get<PokeResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=3',
     );
 
-    data.results.forEach(async ({ name, url }) => {
+    // Forma 1 de evitar esperar en cada inserción.
+    //   Con promesas donde al final se insertan todas de forma simultanea
+    const insertPromisesArray = [];
+
+    data.results.forEach(({ name, url }) => {
       const segments = url.split('/');
       const no: number = +segments[segments.length - 2];
 
-      // Problema: Si hay que insertar muchísimos registros hay que esperar
-      await this.pokemonModel.create({ name, no });
+      // Se evita hacer un await a cada una de las peticiones
+      // Problema: Se hacen múltiples inserciones
+      insertPromisesArray.push(this.pokemonModel.create({ name, no }));
     });
+
+    // Aquí se hacen todas las inserciones de manera simultanea
+    await Promise.all(insertPromisesArray);
 
     return 'Seed Executed';
   }
