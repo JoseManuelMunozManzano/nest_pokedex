fc0f8df5801806bc4c254f98eebaf4c627df50ff
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-08-30 18:26:16 +0200

    Seed de 650 elementos y README actualizado

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -19,24 +19,18 @@ export class SeedService {
     await this.pokemonModel.deleteMany();
 
     const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=3',
+      'https://pokeapi.co/api/v2/pokemon?limit=650',
     );
 
-    // Forma 2 de evitar esperar en cada inserción (FORMA OPTIMA)
-    //   Con arreglos e insertMany (por eso paso Mongoose en vez de PokemonService)
-    //   donde solo se hace una inserción
     const pokemonToInsert: { name: string; no: number }[] = [];
 
     data.results.forEach(({ name, url }) => {
       const segments = url.split('/');
       const no: number = +segments[segments.length - 2];
 
-      // Se evita hacer un await a cada una de las peticiones
-      // Tenemos un arreglo aquí
       pokemonToInsert.push({ name, no });
     });
 
-    // Aquí se hace solo 1 inserción con un montón de entradas, pero solo 1 inserción
     await this.pokemonModel.insertMany(pokemonToInsert);
 
     return 'Seed Executed';
